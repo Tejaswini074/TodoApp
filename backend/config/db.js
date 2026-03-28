9794ff68452050321019361e2fb1088d863795c9
Tejaswini074 <tejaswinis532@gmail.com>
2026-03-28 16:48:59 +0530

    added env

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,16 @@
-const { Sequelize } =require("sequelize");
 
-const sequelize =new Sequelize("TodoApp_db","root","Root@123",{
-    host:"localhost",
-    dialect:"mysql",
-});
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(
+    process.env.MYSQLDATABASE,
+    process.env.MYSQLUSER,
+    process.env.MYSQLPASSWORD,
+    {
+        host: process.env.MYSQLHOST,
+        port: process.env.MYSQLPORT,
+        dialect: 'mysql',
+        logging: false,
+    }
+);
 
 module.exports = sequelize;
